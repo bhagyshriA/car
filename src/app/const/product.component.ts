8873006e10afff19d2665ae232ffe484cee74f70
bhagyshriA <autib16@gmail.com>
2026-08-01 02:47:33 +0530

    On branch main  Your branch is up to date with 'origin/main'. Changes to be committed: 	new file:   src/app/component/product/product.component.scss 	new file:   src/app/const/product.component.ts

---
+++ src/app/const/product.component.ts
@@ -0,0 +1,52 @@
+
+import { Iproduct } from "../model/product.component";
+
+export const products: Array<Iproduct>=[
+
+
+  {
+    pname: 'Maruti Suzuki Baleno',
+    rating: 4.6,
+    img: 'https://imgd.aeplcdn.com/1920x1080/n/cw/ec/102663/baleno-exterior-right-front-three-quarter-69.png',
+    description: 'Premium hatchback featuring a spacious cabin, modern tech dashboard, 360-degree camera, and sleek styling.'
+  },
+  {
+    pname: 'Maruti Suzuki Brezza',
+    rating: 4.7,
+    img: 'https://stimg.cardekho.com/images/carexteriorimages/930x620/Maruti/Vitara-Brezza/7295/1638268460167/front-left-side-47.jpg',
+    description: 'Robust compact SUV equipped with electric sunroof, smart hybrid tech, and high ground clearance.'
+  },
+  {
+    pname: 'Maruti Suzuki Fronx',
+    rating: 4.6,
+    img: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/130591/fronx-exterior-right-front-three-quarter-109.jpeg',
+    description: 'Coupe-styled crossover boasting a futuristic front fascia, turbo boosterjet engine option, and advanced infotainment.'
+  },
+  {
+    pname: 'Maruti Suzuki Grand Vitara',
+    rating: 4.8,
+    img: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/123185/grand-vitara-exterior-right-front-three-quarter-4.jpeg',
+    description: 'Mid-size premium SUV featuring strong hybrid technology, ALLGRIP all-wheel drive system, and panoramic sunroof.'
+  },
+
+  {
+    pname: 'Maruti Suzuki Ertiga',
+    rating: 4.5,
+    img: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/115777/ertiga-exterior-right-front-three-quarter-5.jpeg',
+    description: 'Versatile 7-seater MPV engineered for family comfort, excellent mileage, and flexible seating configurations.'
+  },
+  {
+    pname: 'Maruti Suzuki Dzire',
+    rating: 4.4,
+    img: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/45691/dzire-exterior-right-front-three-quarter-3.jpeg',
+    description: 'Best-selling compact sedan delivering refined performance, elegant exterior lines, and premium rear seat comfort.'
+  },
+
+  {
+    pname: 'Maruti Suzuki Alto K10',
+    rating: 4.2,
+    img: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/127563/alto-k10-exterior-right-front-three-quarter-58.jpeg',
+    description: 'Affordable entry-level hatchback powered by a peppy 1.0L K-Series engine for effortless daily commuting.'
+  }
+];
+
